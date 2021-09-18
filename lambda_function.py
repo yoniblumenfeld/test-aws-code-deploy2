38f097a8f029aba85360e2c510685d6d2a2eab68
Yoni Blumenfeld <yonei06@gmail.com>
2021-09-18 16:51:07 +0300

    wooho

--- lambda_function.py
+++ lambda_function.py
@@ -59,7 +59,7 @@ def lambda_handler(event, context):
     except Exception as e:
         statusCode = 403
         message = f'error: {e}'
-    print('new Version!')
+    print('new Version! 1.1')
     return {
         'entity_name': entity_name,
         'statusCode': statusCode,
